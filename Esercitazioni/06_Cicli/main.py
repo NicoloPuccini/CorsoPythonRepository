df3c44c70d9db9d13268c7a3ef789ecca9eeb520
NicoloPuccini <cordarken90@gmail.com>
2025-03-25 14:46:09 +0100

    cicli

---
+++ Esercitazioni/06_Cicli/main.py
@@ -0,0 +1,15 @@
+# i cicli sono :
+# for
+# while
+#Non esistono do while , ma si possono simulare
+
+#FOR
+
+for i in range(11): #range genera elementi da 0 a 10
+    print(i)
+
+
+#WHILE
+
+while True :
+    print(f"Ciao {i}")
