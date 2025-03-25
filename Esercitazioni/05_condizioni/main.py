e431e8f081a4f1af80c6b590e7927aad329a7cb4
NicoloPuccini <cordarken90@gmail.com>
2025-03-25 12:57:54 +0100

    05_condizioni

---
+++ Esercitazioni/05_condizioni/main.py
@@ -0,0 +1,28 @@
+# CONDIZIONI
+
+
+# non c'è lo switch solo if else ed elif (che è if else)
+num=5
+if num_r < 10 :
+    print(num)
+else:
+    print(num-2)
+
+#elif
+
+if num == 5:
+    print(1)
+elif num==3 :
+    print(2)
+else :
+    print(3)
+
+#Match case (Non si usa quasi mai) ed è simile allo switch
+match y :
+    case 5:
+        print(5)
+    case 10:
+        print(10)
+    case _:     #Questo è il caso di Default in cui non si verificano tutti gli altri
+        print("ne 5 ne 10")
+
