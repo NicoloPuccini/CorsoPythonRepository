69937a916193a8cf560a1dd901ba53a1a1309e2f
NicoloPuccini <cordarken90@gmail.com>
2025-03-26 12:17:40 +0100

    Update 02

--- Compiti/02_Fizz_Buzz/main.py
+++ Compiti/02_Fizz_Buzz/main.py
@@ -17,12 +17,18 @@ fizzBuzz_count = 0
 while True :
     print("\n")
     while True:
-        num = input("inserisci un numero")
-        if num.isdigit():
-            num=int(num)
+        num = input("insert a number or exit ")
+        if num.lower() =="exit":
+            break
+
+        if num.isdigit():   #isdigit() è un metodo di string
+            num=int(num)    #int() è una funzione che converte il valore in variabile intera
             break
         else:
-            print("Please enter only numbers")
+            print("Please enter only numbers or exit")
+
+    if num.lower()=="exit":
+        break
 
     ans = ""
     if num%3==0 :
@@ -41,10 +47,6 @@ while True :
             fizzBuzz_count+=1
     print(f"Fizz -> {fizz_count}\nBuzz -> {buzz_count}\nFizzBuzz -> {fizzBuzz_count}\n")
     
-    endProgram = input("Vuoi continuare [Y/N]")
-    if endProgram.lower == 'y' or endProgram=='Y':
-        break
-
 
 
 
